4d8ffbd975c3446780a3051040b96006029a87a9
stevenhaa <stevenhawork@hotmail.com>
2023-01-01 22:16:56 -0500

    sample hero image

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 import Navbar from './components/Navbar/Navbar';
+import HeroImage from './components/HeroImage/HeroImage';
+
 function App() {
   return (
     <div>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,40 +1,52 @@
-import {Collapse} from 'bootstrap';
-import './Navbar.css'
-import logo from "../../assets/logo.png"
+import { Collapse } from 'bootstrap';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
 
-function Navbar () {
+function Navbar() {
   return (
-      <nav class="navbar navbar-expand-md bg-light navbar-light shadow-sm p-3 mb-5 bg-body rounded">
-        <a href="#" class="navbar-brand">
-          <img src={logo} alt="logo" id="logoimg"/>
-        </a>
-  
-        <button class="text-center navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#coll" >
-          <span class="navbar-toggler-icon"></span>
-          </button> 
+    <nav class='navbar navbar-expand-md bg-light navbar-light shadow-sm p-3 mb-5 bg-body rounded'>
+      <a href='#' class='navbar-brand'>
+        <img src={logo} alt='logo' id='logoimg' />
+      </a>
 
-          <div class="collapse navbar-collapse justify-content-start" id="coll">
-            <ul class="navbar-nav text-center" id="custom-links">
-            <li class="nav-item">
-              <a href="#" class="nav-link">About</a>
-            </li>
+      <button
+        class='text-center navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#coll'
+      >
+        <span class='navbar-toggler-icon'></span>
+      </button>
 
-            <li class="nav-item">
-              <a href="#" class="nav-link">Service</a>
-            </li>
+      <div class='collapse navbar-collapse justify-content-start' id='coll'>
+        <ul class='navbar-nav text-center' id='custom-links'>
+          <li class='nav-item'>
+            <a href='#' class='nav-link'>
+              About
+            </a>
+          </li>
 
-            <li class="nav-item">
-              <a href="#" class="nav-link">Gallery</a>
-            </li>
+          <li class='nav-item'>
+            <a href='#' class='nav-link'>
+              Service
+            </a>
+          </li>
 
-            <li class="nav-item">
-              <a href="#" class="nav-link">Contact Us</a>
-            </li>
-            </ul>
-          </div>
-          </nav>
-  )
+          <li class='nav-item'>
+            <a href='#' class='nav-link'>
+              Gallery
+            </a>
+          </li>
+
+          <li class='nav-item'>
+            <a href='#' class='nav-link'>
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
 
 export default Navbar;

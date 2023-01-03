07271098fa78362372eed66f155b2234ed1a4b4e
Diana Vu <dee@Dianas-MacBook.local>
2023-01-03 15:15:19 -0500

    added a footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'
+
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }

8cfc0acd68ca3ea22b3ab2ab7fc3363520a358cf
stevenhaa <stevenhawork@hotmail.com>
2023-01-03 23:07:16 -0500

    hero image with transition effect

--- client/src/components/HeroImage/HeroImage.js
+++ client/src/components/HeroImage/HeroImage.js
@@ -1,17 +1,18 @@
 import './HeroImage.css';
+import kitchenIsland from '../../assets/kitchenIsland.jpg';
 
 function HeroImage() {
   return (
     <div className='overlay'>
-      <div className='p-5 text-center bg-image rounded-3 hero-image'>
-        <div className='mask'>
-          <div className='d-flex justify-content-center align-items-center h-100'>
-            <div className='text-white'>
-              <h1 className='mb-3'>TOP GROUP CONSTURCTION</h1>
-              <h4 className='mb-3'>We make your house feel more like home</h4>
-            </div>
-          </div>
-        </div>
+      <img src={kitchenIsland} />
+      <div className='d-flex justify-content-center align-items-center'>
+        <h4 className='mb-3 hero-image-title'>
+          <span className='hero-image-corners'>
+            WE MAKE YOUR HOUSE
+            <hr className='hero-image-divider'></hr>
+            FEEL MORE LIKE HOME
+          </span>
+        </h4>
       </div>
     </div>
   );
